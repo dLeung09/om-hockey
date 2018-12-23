bea65afe4aad4ddc41bb7c5d83efbf9618ef62df
David Leung <david.leung@queensu.ca>
2018-12-22 23:14:40 -0500

    Add Rosters page
    Prior to this change, there was no page that showed only the current
    rosters of the teams.
    
    This change adds a Rosters page to show team rosters.

--- src/app/component/roster/roster.component.ts
+++ src/app/component/roster/roster.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { merge, tap } from 'rxjs/operators';
+
+import { Player } from '../../model/player';
+import { Team } from '../../model/team';
+import { PlayersDataSource } from '../../services/players.datasource';
+import { DataService } from '../../services/data.service';
+import { BackendService } from '../../services/backend.service';
 
 @Component({
   selector: 'app-roster',
@@ -7,9 +15,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RosterComponent implements OnInit {
 
-  constructor() { }
+  private teams: Team[] = null;
+
+  private datasources: Array<PlayersDataSource>;
+
+  private columns = [
+    { columnDef: 'name', header: 'Name', cellData: (element: Player) => `${element.name}`, isSortable: true },
+  ];
+
+  constructor(private dataService: DataService, private backendService: BackendService) { }
 
   ngOnInit() {
+    this.datasources = [];
+    this.backendService.getTeams()
+      .subscribe(teams => {
+        teams.forEach(team => {
+          const datasource = new PlayersDataSource(this.dataService);
+          datasource.setSort('asc', 'name');
+          datasource.setFilter('team', team.name);
+          this.datasources.push(datasource);
+          console.log('[DAVID] datasource', datasource);
+        });
+      });
   }
 
 }
